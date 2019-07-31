0d0ef76308854e0f0016f38673ea1ef51119c304
asiemer002 <adam.b.siemer@us.pwc.com>
2019-07-30 22:37:59 -0500

    add new data models

--- imperial_city_beef/erp/admin.py
+++ imperial_city_beef/erp/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Product, InventoryMovement
+from .models import (
+    Product,
+    InventoryMovement,
+    Customer,
+    SalesOrder,
+    SalesOrderDetail
+)
 
 admin.site.register(Product)
 admin.site.register(InventoryMovement)
+admin.site.register(Customer)
+admin.site.register(SalesOrder)
+admin.site.register(SalesOrderDetail)
--- imperial_city_beef/erp/models.py
+++ imperial_city_beef/erp/models.py
@@ -15,3 +15,31 @@ class InventoryMovement(models.Model):
     def __str__(self):
         return f'{self.product} changed by {self.count} units.'
 
+class Customer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    shipping_address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=12)
+
+    def __str__(self):
+        return f'{self.name}'        
+
+class SalesOrder(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+
+    def __str__(self):
+        return f'Sales Order: {self.id}'     
+
+class SalesOrderDetail(models.Model):
+    sales_order = models.ForeignKey(SalesOrder, on_delete=models.CASCADE)    
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)   
+    count = models.IntegerField()
+
+    def __str__(self):
+        return f'Sales Order Detail: {self.sales_order.id}'    
+
+
+
