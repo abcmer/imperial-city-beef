3d63e0b51c064fb74b6383b028c5e63c98da1c35
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2019-08-01 00:27:42 -0500

    add fixtures for initial data

--- imperial_city_beef/imperial_city_beef/settings.py
+++ imperial_city_beef/imperial_city_beef/settings.py
@@ -125,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Fixture directory for seeding the database
+FIXTURE_DIRS = (
+   '../erp/fixtures/',
+)
