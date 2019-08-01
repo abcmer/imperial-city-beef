f6d4782874d8b8f68cbd9971f90b570ae8890313
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2019-08-01 00:05:47 -0500

    finish add to cart functionality

--- imperial_city_beef/erp/views.py
+++ imperial_city_beef/erp/views.py
@@ -23,12 +23,27 @@ def product_detail(request, product_id):
         raise Http404('Product does not exist')
 
 def cart(request, customer_id):
-    cart_items = Cart.objects.filter(customer_id=customer_id).select_related('product') 
-    context = {
-        'cart_items': cart_items,
-        'customer_id': customer_id
-        }
-    return render(request, 'erp/cart.html', context)   
+    if request.method == 'GET':
+        cart_items = Cart.objects.filter(customer_id=customer_id).select_related('product') 
+        context = {
+            'cart_items': cart_items,
+            'customer_id': customer_id
+            }
+        return render(request, 'erp/cart.html', context) 
+
+    if request.method == 'POST':
+        product_id = request.environ['QUERY_STRING'].split('=')[1]
+        product = Product.objects.get(id=product_id)
+
+        cart_add = Cart(customer_id=customer_id, product_id=product_id, count=1)
+        cart_add.save()
+
+        products = Product.objects.all()    
+        context = {
+            'products': products,
+            'product_added_to_cart': product.name
+            }
+        return render(request, 'erp/products.html', context)
 
 
 def checkout(request, customer_id):
